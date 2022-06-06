6fd43c1fa4a4a902d79798006c93a73320167081
alecksandr26 <sansepiol26@gmail.com>
2022-06-06 16:22:28 -0500

    Now I know how to put images

--- staticfiles/staticfiles.py
+++ staticfiles/staticfiles.py
@@ -1,5 +1,5 @@
 # This is an example of how we can manipulate static files in flask
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 
 # Here I define the path of the templates and static files
 app = Flask(__name__)
@@ -7,6 +7,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("base.html")
+    return render_template("base.html", url_for = url_for)
 
 
