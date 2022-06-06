ed9154fa4842813df524dcbff5b9c65114a49328
alecksandr26 <sansepiol26@gmail.com>
2022-06-06 15:50:55 -0500

    Finish with the navbar example

--- navbar/navbar.py
+++ navbar/navbar.py
@@ -1,11 +1,26 @@
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 
 app = Flask(__name__)
 
 
+links = []
+
+def getName(func):
+    global links
+    links.append(func.__name__)
+    return func
+    
+
+@app.route('/blog')
+@getName
+def blog():
+    return render_template('blog.html', url_for = url_for, links = links)
+    
+
 
 @app.route('/')
+@getName
 def root():
-    return render_template('navbar.html')
+    return render_template('navbar.html', url_for = url_for, links = links)
 
