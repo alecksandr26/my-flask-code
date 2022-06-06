21b65f9c785aed3d10b4bdeadda9cbdc87aa7d56
alecksandr26 <sansepiol26@gmail.com>
2022-06-06 12:01:56 -0500

    Working with code of block

---
+++ templates/inheritance.py
@@ -0,0 +1,19 @@
+"""
+This is an exmple of how can do inheritance with the templates
+"""
+
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+todos = ["Clean my room", "Write the subroutine", "Fix the bug"]
+
+
+@app.route('/')
+def root():
+    
+    # I pass the todos
+    return render_template("base.html", todos = todos)
+
