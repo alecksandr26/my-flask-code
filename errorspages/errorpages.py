6681ea7695dd55bf6ead78d122877428b21a8460
alecksandr26 <sansepiol26@gmail.com>
2022-06-11 11:27:20 -0500

    New version of the paga

--- errorspages/errorpages.py
+++ errorspages/errorpages.py
@@ -5,10 +5,16 @@ from flask import Flask, request, render_template
 app = Flask(__name__)
 
 
+
+# Just like this we can deal with the different type of errors
+@app.errorhandler(404)
+def error_404():
+    return render_template('pagenotfound.html')
+
+
 @app.route('/hello')
 def hello():
     return 'Hello'
-
 
 
 @app.route('/')
