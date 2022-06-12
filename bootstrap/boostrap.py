0aa6b00cb69480a0e85c439802722055b07172bc
alecksandr26 <sansepiol26@gmail.com>
2022-06-11 19:46:34 -0500

    Dealing with bootstrap

--- bootstrap/boostrap.py
+++ bootstrap/boostrap.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template
 
+# Here I import the module bootstrap
+from flask_bootstrap import Bootstrap
+
 app = Flask(__name__)
 
+# Here with this we give access to our files 
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
