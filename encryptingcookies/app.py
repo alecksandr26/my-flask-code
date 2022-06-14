2a35c3f58faaf95be46062a671b2e6fb1476bfe2
alecksandr26 <sansepiol26@gmail.com>
2022-06-14 11:33:51 -0500

    Finishing with the cookie encryption

--- encryptingcookies/app.py
+++ encryptingcookies/app.py
@@ -1,15 +1,28 @@
-from flask import Flask
+from flask import Flask, request, session, make_response, redirect
 
 
 app = Flask(__name__)
 
+# To have a secret key
+app.config['SECRET_KEY'] = 'mykey'
+
+@app.route('/hello')
+def hello():
+    # Try to fecth the data
+    if not session.get('user_ip'):
+        return 'We need an ip'
+    
+    return f"Hello {session.get('user_ip')}"
+    
 
 @app.route('/')
 def index():
-    return 'hello'
+    response = make_response(redirect('/hello'))
+    
+    # Store my value 
+    session['user_ip'] = request.remote_addr
 
-
-
+    return response
 
 
 
