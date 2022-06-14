596004c02f6a045d397f2d3d48f6b9740885c7c7
alecksandr26 <sansepiol26@gmail.com>
2022-06-14 13:15:11 -0500

    A simple login page

--- formsbootstrap/app.py
+++ formsbootstrap/app.py
@@ -2,13 +2,50 @@
 We are going to create a simple login 
 """
 
-from flask import Flask, request, make_response, session, redirect, render_template
+from flask import Flask, request, make_response, session, redirect, render_template, url_for
+from flask_bootstrap import Bootstrap
+
+# import the flask class
+from flask_wtf import FlaskForm
+# The reference functions
+from wtforms.fields import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+bootstrap  = Bootstrap(app)
 
 app.config["SECRET_KEY"] = 'mykey'
 
+class LoginForm(FlaskForm):
+    username = StringField("User name: ", validators = [DataRequired()])
+    password = PasswordField("User password", validators = [DataRequired()])
+    submit = SubmitField('Send')
 
-@app.route('/')
-def index():
-    return 'hello'
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    # I create a contex to pass all the argumetns
+    contex = {
+        'url_for' : url_for,
+        'login_form' : LoginForm() # Creates an instance
+    }
+
+    if contex['login_form'].validate_on_submit():
+        # Get the data
+        username = contex['login_form'].username.data
+        password = contex['login_form'].password.data
+        session['user'] = { 'username' : username, 'password' : password }
+        return make_response(redirect('/'))
+    
+    return render_template('login.html', **contex)
+
+
+@app.route('/', methods = ['GET'])
+def home():
+    if not session.get('user'):
+        return  make_response(redirect('/login'))
+
+    contex = {
+        'user' : session.get('user')
+    }
+    
+    return render_template('home.html', **contex)
